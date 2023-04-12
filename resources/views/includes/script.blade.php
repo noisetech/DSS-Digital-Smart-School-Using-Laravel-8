1c21432239d4ffe445211d3c8f5a41492d45cf37
noisetech <box92143@gmail.com>
2023-04-12 09:10:43 +0700

    4-12-203

---
+++ resources/views/includes/script.blade.php
@@ -0,0 +1,33 @@
+<!-- General JS Scripts -->
+<script src="{{ asset('be/dist/assets/modules/jquery.min.js') }}"></script>
+<script src="{{ asset('be/dist/assets/modules/popper.js') }}"></script>
+<script src="{{ asset('be/dist/assets/modules/tooltip.js') }}"></script>
+<script src="{{ asset('be/dist/assets/modules/bootstrap/js/bootstrap.min.js') }}"></script>
+<script src="{{ asset('be/dist/assets/modules/nicescroll/jquery.nicescroll.min.js') }}"></script>
+<script src="{{ asset('be/dist/assets/modules/moment.min.js') }}"></script>
+<script src="{{ asset('be/dist/assets/js/stisla.js') }}"></script>
+
+<!-- JS Libraies -->
+<script src="{{ asset('be/dist/assets/modules/simple-weather/jquery.simpleWeather.min.js') }}"></script>
+<script src="{{ asset('be/dist/assets/modules/chart.min.js') }}"></script>
+<script src="{{ asset('be/dist/assets/modules/jqvmap/dist/jquery.vmap.min.js') }}"></script>
+<script src="{{ asset('be/dist/assets/modules/jqvmap/dist/maps/jquery.vmap.world.js') }}"></script>
+<script src="{{ asset('be/dist/assets/modules/summernote/summernote-bs4.js') }}"></script>
+<script src="{{ asset('be/dist/assets/modules/chocolat/dist/js/jquery.chocolat.min.js') }}"></script>
+<script src="https://cdn.jsdelivr.net/npm/select2@4.0.13/dist/js/select2.min.js"></script>
+
+<script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.13.4/js/dataTables.bootstrap4.min.js"></script>
+<script src="https://cdn.datatables.net/responsive/2.4.1/js/dataTables.responsive.min.js"></script>
+<script src="https://cdn.datatables.net/responsive/2.4.1/js/responsive.bootstrap4.min.js"></script>
+
+<script src="{{ asset('be/sweetalert.js') }}"></script>
+
+<!-- Page Specific JS File -->
+<script src="{{ asset('be/dist/assets/js/page/index-0.js') }}"></script>
+
+<!-- Template JS File -->
+<script src="{{ asset('be/dist/assets/js/scripts.js') }}"></script>
+<script src="{{ asset('be/dist/assets/js/custom.js') }}"></script>
+
+@stack('script')
